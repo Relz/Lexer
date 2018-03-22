2ccaacfb767b28d6975a9288a54b2e315d92880b
Starovoytov Alexey <relz0071@gmail.com>
2018-03-23 00:31:43 +0300

    Добавлен тест ключевого слова "return"

---
+++ LexerTest/tests/ReturnTokenTest.cpp
@@ -0,0 +1,55 @@
+#include "TestHelper.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+
+TEST(return_token, determining_if_stay_alone)
+{
+	ExpectTokens("return", { Token::RETURN });
+}
+
+TEST(return_token, determining_if_stay_between_delimiters)
+{
+	ExpectTokens(" return ", { Token::RETURN });
+	ExpectTokens(";return;", { Token::SEMICOLON, Token::RETURN, Token::SEMICOLON });
+}
+
+TEST(return_token, determining_if_stay_near_delimiter)
+{
+	ExpectTokens("return;", { Token::RETURN, Token::SEMICOLON });
+	ExpectTokens(";return", { Token::SEMICOLON, Token::RETURN });
+}
+
+TEST(return_token, not_determining_if_between_numbers)
+{
+	ExpectTokens("1return1", { Token::UNKNOWN });
+	ExpectTokens("1return1.1", { Token::UNKNOWN });
+	ExpectTokens("1.1return1", { Token::UNKNOWN });
+	ExpectTokens("1.1return1.1", { Token::UNKNOWN });
+	ExpectTokens("1E+1return1", { Token::UNKNOWN });
+	ExpectTokens("1return1E+1", { Token::UNKNOWN });
+}
+
+TEST(return_token, not_determining_if_part_of_string_literal)
+{
+	ExpectTokens("\"return\"", { Token::STRING_LITERAL });
+	ExpectTokens("\" return \"", { Token::STRING_LITERAL });
+	ExpectTokens("\"1return1\"", { Token::STRING_LITERAL });
+	ExpectTokens("\";return;\"", { Token::STRING_LITERAL });
+}
+
+TEST(return_token, not_determining_if_part_of_comment)
+{
+	ExpectTokens("//return", { });
+	ExpectTokens("// return ", { });
+	ExpectTokens("//1return1", { });
+	ExpectTokens("//;return;", { });
+	ExpectTokens("/*return*/", { });
+	ExpectTokens("/* return */", { });
+	ExpectTokens("/*1return1*/", { });
+	ExpectTokens("/*;return;*/", { });
+	ExpectTokens("/*return", { });
+	ExpectTokens("/* return ", { });
+	ExpectTokens("/*1return1", { });
+	ExpectTokens("/*;return;", { });
+}
