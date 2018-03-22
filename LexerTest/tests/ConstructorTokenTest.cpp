d9c0d9fcfe2745a2d3b2d53642f15cbf88efb21d
Starovoytov Alexey <relz0071@gmail.com>
2018-03-23 00:10:34 +0300

    Добавлен тест ключевого слова "Constructor"

---
+++ LexerTest/tests/ConstructorTokenTest.cpp
@@ -0,0 +1,55 @@
+#include "TestHelper.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+
+TEST(constructor_token, determining_if_stay_alone)
+{
+	ExpectTokens("Constructor", { Token::CONSTRUCTOR });
+}
+
+TEST(constructor_token, determining_if_stay_between_delimiters)
+{
+	ExpectTokens(" Constructor ", { Token::CONSTRUCTOR });
+	ExpectTokens(";Constructor;", { Token::SEMICOLON, Token::CONSTRUCTOR, Token::SEMICOLON });
+}
+
+TEST(constructor_token, determining_if_stay_near_delimiter)
+{
+	ExpectTokens("Constructor;", { Token::CONSTRUCTOR, Token::SEMICOLON });
+	ExpectTokens(";Constructor", { Token::SEMICOLON, Token::CONSTRUCTOR });
+}
+
+TEST(constructor_token, not_determining_if_between_numbers)
+{
+	ExpectTokens("1Constructor1", { Token::UNKNOWN });
+	ExpectTokens("1Constructor1.1", { Token::UNKNOWN });
+	ExpectTokens("1.1Constructor1", { Token::UNKNOWN });
+	ExpectTokens("1.1Constructor1.1", { Token::UNKNOWN });
+	ExpectTokens("1E+1Constructor1", { Token::UNKNOWN });
+	ExpectTokens("1Constructor1E+1", { Token::UNKNOWN });
+}
+
+TEST(constructor_token, not_determining_if_part_of_string_literal)
+{
+	ExpectTokens("\"Constructor\"", { Token::STRING_LITERAL });
+	ExpectTokens("\" Constructor \"", { Token::STRING_LITERAL });
+	ExpectTokens("\"1Constructor1\"", { Token::STRING_LITERAL });
+	ExpectTokens("\";Constructor;\"", { Token::STRING_LITERAL });
+}
+
+TEST(constructor_token, not_determining_if_part_of_comment)
+{
+	ExpectTokens("//Constructor", { });
+	ExpectTokens("// Constructor ", { });
+	ExpectTokens("//1Constructor1", { });
+	ExpectTokens("//;Constructor;", { });
+	ExpectTokens("/*Constructor*/", { });
+	ExpectTokens("/* Constructor */", { });
+	ExpectTokens("/*1Constructor1*/", { });
+	ExpectTokens("/*;Constructor;*/", { });
+	ExpectTokens("/*Constructor", { });
+	ExpectTokens("/* Constructor ", { });
+	ExpectTokens("/*1Constructor1", { });
+	ExpectTokens("/*;Constructor;", { });
+}
