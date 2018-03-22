930e2427b2f0ec98a3147d012470fd314b4ab237
Starovoytov Alexey <relz0071@gmail.com>
2018-03-23 00:17:37 +0300

    Добавлен тест ключевого слова "public"

---
+++ LexerTest/tests/PublicTokenTest.cpp
@@ -0,0 +1,55 @@
+#include "TestHelper.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+
+TEST(public_token, determining_if_stay_alone)
+{
+	ExpectTokens("public", { Token::PUBLIC });
+}
+
+TEST(public_token, determining_if_stay_between_delimiters)
+{
+	ExpectTokens(" public ", { Token::PUBLIC });
+	ExpectTokens(";public;", { Token::SEMICOLON, Token::PUBLIC, Token::SEMICOLON });
+}
+
+TEST(public_token, determining_if_stay_near_delimiter)
+{
+	ExpectTokens("public;", { Token::PUBLIC, Token::SEMICOLON });
+	ExpectTokens(";public", { Token::SEMICOLON, Token::PUBLIC });
+}
+
+TEST(public_token, not_determining_if_between_numbers)
+{
+	ExpectTokens("1public1", { Token::UNKNOWN });
+	ExpectTokens("1public1.1", { Token::UNKNOWN });
+	ExpectTokens("1.1public1", { Token::UNKNOWN });
+	ExpectTokens("1.1public1.1", { Token::UNKNOWN });
+	ExpectTokens("1E+1public1", { Token::UNKNOWN });
+	ExpectTokens("1public1E+1", { Token::UNKNOWN });
+}
+
+TEST(public_token, not_determining_if_part_of_string_literal)
+{
+	ExpectTokens("\"public\"", { Token::STRING_LITERAL });
+	ExpectTokens("\" public \"", { Token::STRING_LITERAL });
+	ExpectTokens("\"1public1\"", { Token::STRING_LITERAL });
+	ExpectTokens("\";public;\"", { Token::STRING_LITERAL });
+}
+
+TEST(public_token, not_determining_if_part_of_comment)
+{
+	ExpectTokens("//public", { });
+	ExpectTokens("// public ", { });
+	ExpectTokens("//1public1", { });
+	ExpectTokens("//;public;", { });
+	ExpectTokens("/*public*/", { });
+	ExpectTokens("/* public */", { });
+	ExpectTokens("/*1public1*/", { });
+	ExpectTokens("/*;public;*/", { });
+	ExpectTokens("/*public", { });
+	ExpectTokens("/* public ", { });
+	ExpectTokens("/*1public1", { });
+	ExpectTokens("/*;public;", { });
+}
