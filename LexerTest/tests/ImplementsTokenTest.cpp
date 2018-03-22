9a1ff49366184ceb91c76e73ccf29170d9ae6ba2
Starovoytov Alexey <relz0071@gmail.com>
2018-03-23 00:07:16 +0300

    Добавлен тест ключевого слова "implements"

---
+++ LexerTest/tests/ImplementsTokenTest.cpp
@@ -0,0 +1,55 @@
+#include "TestHelper.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+
+TEST(implements_token, determining_if_stay_alone)
+{
+	ExpectTokens("implements", { Token::IMPLEMENTS });
+}
+
+TEST(implements_token, determining_if_stay_between_delimiters)
+{
+	ExpectTokens(" implements ", { Token::IMPLEMENTS });
+	ExpectTokens(";implements;", { Token::SEMICOLON, Token::IMPLEMENTS, Token::SEMICOLON });
+}
+
+TEST(implements_token, determining_if_stay_near_delimiter)
+{
+	ExpectTokens("implements;", { Token::IMPLEMENTS, Token::SEMICOLON });
+	ExpectTokens(";implements", { Token::SEMICOLON, Token::IMPLEMENTS });
+}
+
+TEST(implements_token, not_determining_if_between_numbers)
+{
+	ExpectTokens("1implements1", { Token::UNKNOWN });
+	ExpectTokens("1implements1.1", { Token::UNKNOWN });
+	ExpectTokens("1.1implements1", { Token::UNKNOWN });
+	ExpectTokens("1.1implements1.1", { Token::UNKNOWN });
+	ExpectTokens("1E+1implements1", { Token::UNKNOWN });
+	ExpectTokens("1implements1E+1", { Token::UNKNOWN });
+}
+
+TEST(implements_token, not_determining_if_part_of_string_literal)
+{
+	ExpectTokens("\"implements\"", { Token::STRING_LITERAL });
+	ExpectTokens("\" implements \"", { Token::STRING_LITERAL });
+	ExpectTokens("\"1implements1\"", { Token::STRING_LITERAL });
+	ExpectTokens("\";implements;\"", { Token::STRING_LITERAL });
+}
+
+TEST(implements_token, not_determining_if_part_of_comment)
+{
+	ExpectTokens("//implements", { });
+	ExpectTokens("// implements ", { });
+	ExpectTokens("//1implements1", { });
+	ExpectTokens("//;implements;", { });
+	ExpectTokens("/*implements*/", { });
+	ExpectTokens("/* implements */", { });
+	ExpectTokens("/*1implements1*/", { });
+	ExpectTokens("/*;implements;*/", { });
+	ExpectTokens("/*implements", { });
+	ExpectTokens("/* implements ", { });
+	ExpectTokens("/*1implements1", { });
+	ExpectTokens("/*;implements;", { });
+}
