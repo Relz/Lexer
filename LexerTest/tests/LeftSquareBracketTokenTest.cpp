326ae8771da4baa92fb5da5e6ce2e0357e78dbe5
Starovoytov Alexey <relz0071@gmail.com>
2018-04-02 10:00:00 +0300

    Добавлен тест определения квадратной открывающей скобки

---
+++ LexerTest/tests/LeftSquareBracketTokenTest.cpp
@@ -0,0 +1,60 @@
+#include "Lexer/Token/Token.h"
+#include "TestHelper.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+
+TEST(left_square_bracket_token, determining_if_stay_alone)
+{
+	ExpectTokens("[", { Token::LEFT_SQUARE_BRACKET });
+}
+
+TEST(left_square_bracket_token, determining_if_stay_between_delimiters)
+{
+	ExpectTokens(" [ ", { Token::LEFT_SQUARE_BRACKET });
+	ExpectTokens(";[;", { Token::SEMICOLON, Token::LEFT_SQUARE_BRACKET, Token::SEMICOLON });
+}
+
+TEST(left_square_bracket_token, determining_if_stay_near_delimiter)
+{
+	ExpectTokens("[;", { Token::LEFT_SQUARE_BRACKET, Token::SEMICOLON });
+	ExpectTokens(";[", { Token::SEMICOLON, Token::LEFT_SQUARE_BRACKET });
+}
+
+TEST(left_square_bracket_token, not_determining_if_between_numbers)
+{
+	ExpectTokens("1[1", { Token::INTEGER, Token::LEFT_SQUARE_BRACKET, Token::INTEGER });
+	ExpectTokens("1.1[1", { Token::FLOAT, Token::LEFT_SQUARE_BRACKET, Token::INTEGER });
+	ExpectTokens("1[1.1", { Token::INTEGER, Token::LEFT_SQUARE_BRACKET, Token::FLOAT });
+	ExpectTokens("1_E+1[1", { Token::EXPONENTIAL, Token::LEFT_SQUARE_BRACKET, Token::INTEGER });
+	ExpectTokens("1[1_E+1", { Token::INTEGER, Token::LEFT_SQUARE_BRACKET, Token::EXPONENTIAL });
+}
+
+TEST(left_square_bracket_token, not_determining_if_in_character_literal)
+{
+	ExpectTokens("'['", { Token::CHARACTER_LITERAL });
+}
+
+TEST(left_square_bracket_token, not_determining_if_part_of_string_literal)
+{
+	ExpectTokens("\"[\"", { Token::STRING_LITERAL });
+	ExpectTokens("\" [ \"", { Token::STRING_LITERAL });
+	ExpectTokens("\"1[1\"", { Token::STRING_LITERAL });
+	ExpectTokens("\";[;\"", { Token::STRING_LITERAL });
+}
+
+TEST(left_square_bracket_token, not_determining_if_part_of_comment)
+{
+	ExpectTokens("//[", { });
+	ExpectTokens("// [ ", { });
+	ExpectTokens("//1[1", { });
+	ExpectTokens("//;[;", { });
+	ExpectTokens("/*[*/", { });
+	ExpectTokens("/* [ */", { });
+	ExpectTokens("/*1[1*/", { });
+	ExpectTokens("/*;[;*/", { });
+	ExpectTokens("/*[", { });
+	ExpectTokens("/* [ ", { });
+	ExpectTokens("/*1[1", { });
+	ExpectTokens("/*;[;", { });
+}
